f370c83423f67e2335857f6bb488c77515e22056
icymane <egor.andrianov34@gmail.com>
2020-05-09 09:45:18 +0700

    Add make

---
+++ CMakeFiles/100Matches.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/100Matches.dir/100MATCHES.c.o"
+  "100Matches.pdb"
+  "100Matches"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/100Matches.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
